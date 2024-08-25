9a64d6ee13cf8edd68a908e5492839ad9e947bed
Henry Dustin <henrydustin95@gmail.com>
2024-08-25 10:10:31 +0100

    finished cashing

--- app/(storefront)/product/[id]/page.tsx
+++ app/(storefront)/product/[id]/page.tsx
@@ -5,6 +5,7 @@ import { ShoppingBagButton } from '@/app/components/SubmitButtons';
 import prisma from '@/app/lib/db';
 import { StarIcon } from 'lucide-react';
 import { notFound } from 'next/navigation';
+import { unstable_noStore as noStore } from 'next/cache';
 
 async function getData(productId: string) {
   const data = await prisma.product.findUnique({
@@ -32,6 +33,7 @@ export default async function ProductIdRoute({
 }: {
   params: { id: string };
 }) {
+  noStore();
   const data = await getData(params.id);
   const addProducttoShoppingCart = addItem.bind(null, data.id);
   return (
